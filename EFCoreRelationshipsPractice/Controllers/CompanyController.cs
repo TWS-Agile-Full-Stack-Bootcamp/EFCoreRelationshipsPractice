8092b50122dd90192ee553fcef9a02b11c4b690a
ChenXu <xuchen@thoughtworks.com>
2020-11-10 23:16:51 +0800

    feat: add async to controller

--- EFCoreRelationshipsPractice/Controllers/CompanyController.cs
+++ EFCoreRelationshipsPractice/Controllers/CompanyController.cs
@@ -14,47 +14,15 @@ namespace EFCoreRelationshipsPractice.Controllers
     public class CompanyController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Company> List()
+        public async Task<IEnumerable<Company>> List()
         {
             return null;
         }
 
         [HttpPost]
-        public Company Add(Company company)
+        public async Task<Company> Add(Company company)
         {
             return null;
         }
-
-        //// GET: api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
